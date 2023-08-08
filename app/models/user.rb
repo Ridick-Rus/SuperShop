6a175748f4c819736c260e2f12602758f533efc9
Andrey Prilepsky <a.prilepsky@sftpro.ru>
2023-08-08 11:23:58 +0300

    update user validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true, presence: true
+  validates :reset_password_token, uniqueness: true
 end
