7cbab084b5e9d4a0f26d69f7048a7ec510f1d2fd
Andrey Prilepsky <a.prilepsky@sftpro.ru>
2023-10-12 11:41:04 +0300

    add products spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    subject { build(:product) }
+
+    let(:invalid_price) { -10 }
+
+    it { is_expected.to have_one(:storage) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_length_of(:description) }
+    it { is_expected.not_to allow_value(invalid_price).for(:price) }
+  end
 end
